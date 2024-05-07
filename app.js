df6f1a898714a61ecc855092db053c337247575f
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-07 20:05:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 const port = 8080;
 const puppeteer = require('puppeteer');
 // const executablePath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux', 'chrome.exe');
-const executablePath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux', 'chrome');
+// const executablePath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux', 'chrome);
 
 const fs = require('fs');
-const directoryPath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux64', 'chrome');
+const directoryPath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux64');
 
 
     fs.readdir(directoryPath, (err, files) => {
@@ -24,25 +24,25 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-const puppeteerSession = async () => {
-    try {
-        const browserInstance = await puppeteer.launch({
-            headless:true,
-            executablePath,
-        });
-        const page = await browserInstance.newPage();
-        await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
-        const items = await page.evaluate(() => {
-            const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
-            return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
-        });
-        console.log(items.length);
+// const puppeteerSession = async () => {
+//     try {
+//         const browserInstance = await puppeteer.launch({
+//             headless:true,
+//             executablePath,
+//         });
+//         const page = await browserInstance.newPage();
+//         await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
+//         const items = await page.evaluate(() => {
+//             const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
+//             return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
+//         });
+//         console.log(items.length);
         
     
-    } catch(err) {
-    console.log(err);
-    }
-}
+//     } catch(err) {
+//     console.log(err);
+//     }
+// }
 
 
-puppeteerSession();
+// puppeteerSession();
