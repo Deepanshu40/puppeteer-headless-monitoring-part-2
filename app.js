1d51bca098bbc654c741e9c9a10eddb8a03359b5
Deepanshu40 <gargdeepanshu40@gmail.com>
2024-05-07 17:22:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express  = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
+const puppeteer = require('puppeteer');
+const executablePath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'win64-124.0.6367.91', 'chrome-win64', 'chrome.exe');
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+const puppeteerSession = async () => {
+    try {
+        const browserInstance = await puppeteer.launch({
+            headless:true,
+            executablePath,
+        });
+        const page = await browserInstance.newPage();
+        await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
+        const items = await page.evaluate(() => {
+            const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
+            return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
+        });
+        console.log(items.length);
+        
+    
+    } catch(err) {
+    console.log(err);
+    }
+}
+
+
+puppeteerSession();
