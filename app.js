846a3222e18de6e310958dd987339c4deb19184b
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-07 20:50:00 +0530

    seems should work

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const path = require('path');
 const app = express();
 const port = 8080;
 const puppeteer = require('puppeteer');
-const executablePath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux', 'chrome.exe');
-const fs = require('fs');
+// const executablePath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux', 'chrome.exe');
+// const fs = require('fs');
 const directoryPath = path.join(__dirname, '.cache', 'puppeteer', 'chrome', 'linux-124.0.6367.91', 'chrome-linux64', 'chrome');
 
 
-    fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-            console.error('Failed to list directory contents:', err);
-        }
-        console.log(files);
-    });
+    // fs.readdir(directoryPath, (err, files) => {
+    //     if (err) {
+    //         console.error('Failed to list directory contents:', err);
+    //     }
+    //     console.log(files);
+    // });
 
 
 
@@ -21,25 +21,25 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-// const puppeteerSession = async () => {
-//     try {
-//         const browserInstance = await puppeteer.launch({
-//             headless:true,
-//             executablePath,
-//         });
-//         const page = await browserInstance.newPage();
-//         await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
-//         const items = await page.evaluate(() => {
-//             const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
-//             return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
-//         });
-//         console.log(items.length);
+const puppeteerSession = async () => {
+    try {
+        const browserInstance = await puppeteer.launch({
+            headless:true,
+            executablePath:directoryPath,
+        });
+        const page = await browserInstance.newPage();
+        await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
+        const items = await page.evaluate(() => {
+            const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
+            return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
+        });
+        console.log(items.length);
         
     
-//     } catch(err) {
-//     console.log(err);
-//     }
-// }
+    } catch(err) {
+    console.log(err);
+    }
+}
 
 
-// puppeteerSession();
+puppeteerSession();
