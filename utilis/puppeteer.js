544f477a83c1ad7701bc81b589394c3f81f52c9b
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-09 05:29:17 +0530

    Update puppeteer.js

--- utilis/puppeteer.js
+++ utilis/puppeteer.js
@@ -36,21 +36,21 @@ module.exports.puppeteerSession = async () => {
     });
 
     
-    // try {
-    //     const browserInstance = await puppeteer.launch({
-    //         headless:true,
-    //         executablePath:prodExecutablePath,
-    //     });
-    //     const page = await browserInstance.newPage();
-    //     await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
-    //     const items = await page.evaluate(() => {
-    //         const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
-    //         return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
-    //     });
-    //     console.log(items.length);
+    try {
+        const browserInstance = await puppeteer.launch({
+            headless:true,
+            executablePath:prodExecutablePath,
+        });
+        const page = await browserInstance.newPage();
+        await page.goto('https://en.wikipedia.org/wiki/Ram_Charan', { waitUntil: 'networkidle0' });
+        const items = await page.evaluate(() => {
+            const paragraphs = Array.from(document.querySelectorAll('p')); // Convert NodeList to Array
+            return paragraphs.map(p => p.innerText); // Map over the array to return innerText of each paragraph
+        });
+        console.log(items.length);
         
     
-    // } catch(err) {
-    // console.log(err);
-    // }
+    } catch(err) {
+    console.log(err);
+    }
 }
